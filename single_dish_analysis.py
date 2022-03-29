1e6d6afa57fa3bd2329ad948077a885cb6de2b24
Sravan Kodali <79488137+sravankodali@users.noreply.github.com>
2022-03-29 19:37:02 -0400

    Update single_dish_analysis.py

--- single_dish_analysis.py
+++ single_dish_analysis.py
@@ -1,21 +1,18 @@
-# import the necessary packages
 from imutils.video import VideoStream
 import argparse
 import datetime
 import imutils
 import time
 import cv2
-# construct the argument parser and parse the arguments
+
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", help=r"c:\Users\Edited_2.mp4")
 ap.add_argument("-a", "--min-area", type=int, default=700, help="1000")
 args = vars(ap.parse_args())
-# if the video argument is None, then we are reading from webcam
 if args.get("video", None) is None:
   print("Running from webcam")
   vs = VideoStream(src=0).start()
   time.sleep(5.0)
-# otherwise, we are reading from a video file
 else:
     print ("Running from video file")
     vs = cv2.VideoCapture(args["video"])
@@ -27,10 +24,7 @@ mating_confirmed = []
 append_counter = 0
 
 
-# loop over the frames of the video
 while True:
-    # grab the current frame and initialize the occupied/unoccupied
-    # text
     frame = vs.read()
     frame = frame if args.get("video", None) is None else frame[1]
     text = "Possibly Mating"
@@ -43,8 +37,6 @@ while True:
             mating_confirmed.append(time.time() - start_time)
             append_counter +=1
 
-    # if the frame could not be grabbed, then we have reached the end
-    # of the video
     if frame is None:
         break
     # resize the frame, convert it to grayscale, and blur it
@@ -56,25 +48,18 @@ while True:
         firstFrame = gray
         continue
 
-        # compute the absolute difference between the current frame and
-    # first frame
+        # compute the absolute difference between the current frame and first frame
     frameDelta = cv2.absdiff(firstFrame, gray)
     thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
-    # dilate the thresholded image to fill in holes, then find contours
-    # on thresholded image
     thresh = cv2.dilate(thresh, None, iterations=2)
     cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
         cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
-    # loop over the contours
     for c in cnts:
-        # if the contour is too small or too big, ignore it
         if cv2.contourArea(c) < args["min_area"]:
             continue
         if cv2.contourArea(c) > 1200: 
             continue
-        # compute the bounding box for the contour, draw it on the frame,
-        # and update the text
         (x, y, w, h) = cv2.boundingRect(c)
         if x > 60 and x < 370:
             if y > 20: 
@@ -83,20 +68,19 @@ while True:
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
                 text = "Not Mating"
                 mating_counter = 0
-            # draw the text and timestamp on the frame
     cv2.putText(frame, "Fly Status: {}".format(text), (10, 20),
         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
     cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
         (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-    # show the frame and record if the user presses a key
-    cv2.imshow("Security Feed", frame)
+
+    cv2.imshow("Primary", frame)
     cv2.imshow("Thresh", thresh)
     cv2.imshow("Frame Delta", frameDelta)
     key = cv2.waitKey(1) & 0xFF
-    # if the `q` key is pressed, break from the lop
+
     if key == ord("q"):
         break
-# cleanup the camera and close any open windows
+
 vs.stop() if args.get("video", None) is None else vs.release()
 cv2.destroyAllWindows()
 video_time = int(input("Please enter the video time in seconds: "))
